0cb334dfe0a6c5786531a997d95394e86de0882c
Haykaz Sayadyan <54286866+hsayadyan@users.noreply.github.com>
2021-06-10 15:34:55 -0700

    getting well list form service, timepicker

--- src/app/components/production-monitoring/production-monitoring.component.ts
+++ src/app/components/production-monitoring/production-monitoring.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Well } from 'src/app/models/well';
+import { ProductionMonitoringService } from 'src/app/services/productionMonitoring.service';
 
 @Component({
   selector: 'app-monitoring',
@@ -8,13 +11,25 @@ import {FormGroup, FormControl } from '@angular/forms';
 })
 export class ProductionMonitoringComponent implements OnInit {
   selected = 'Well1';
+  userId: string = "";
+wells : Well[];
+
 
   range = new FormGroup({
     start: new FormControl(),
     end: new FormControl()
   });
 
-  constructor() { }
+  constructor(
+    private _pmService: ProductionMonitoringService
+    ) { }
+
+  getWells() : void
+  {
+    this._pmService.getWells().subscribe((data: Well[])=>{
+      this.wells = data;
+    });
+  }
 
   onClickCompare(){
     alert('Compare');
@@ -37,6 +52,7 @@ alert(name);
 }
 
   ngOnInit(): void {
+    this.getWells();
   }
 
 }
