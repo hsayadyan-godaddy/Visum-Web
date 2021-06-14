436b6cd01ad9921fe5435dbaae7b61d63ae47175
Haykaz Sayadyan <54286866+hsayadyan@users.noreply.github.com>
2021-06-14 15:27:51 -0700

    created components for production monitoring page

---
+++ src/app/shared/components/monitoring-charts/monitoring-charts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monitoring-charts',
+  templateUrl: './monitoring-charts.component.html',
+  styleUrls: ['./monitoring-charts.component.css']
+})
+export class MonitoringChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/components/well-sellection-with-buttons/well-sellection-with-buttons.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Productionmonitoring } from 'src/app/models/productionmonitoring/productionmonitoring';
+import { Well } from 'src/app/models/well';
+import { ProductionMonitoringService } from 'src/app/services/productionMonitoring.service';
+
+@Component({
+  selector: 'app-well-sellection-with-buttons',
+  templateUrl: './well-sellection-with-buttons.component.html',
+  styleUrls: ['./well-sellection-with-buttons.component.css']
+})
+export class WellSellectionWithButtonsComponent implements OnInit {
+  selected = 'Well1';
+  wells : Well[];
+  pmData : Productionmonitoring;
+
+  constructor(
+    private _pmService: ProductionMonitoringService
+    ) { }
+
+  getWells() : void
+  {
+    this._pmService.getWells().subscribe((data: Well[])=>{
+      this.wells = data;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+
+
+  onClickCompare(){
+    alert('Compare');
+  }
+
+  onClickComment(){
+    alert('Comment');
+  }
+
+  onClickDisplay(){
+    alert('Display');
+  }
+
+  onClickChart(){
+    alert('Chart');
+  }
+}
