3880e66e764258b950685f32d4b5bc6c466e935e
Haykaz Sayadyan <54286866+hsayadyan@users.noreply.github.com>
2021-06-22 14:34:23 -0700

    class with constants

--- src/app/services/productionMonitoring.service.ts
+++ src/app/services/productionMonitoring.service.ts
@@ -6,6 +6,7 @@ import { environment } from 'src/environments/environment';
 import { DepthType } from '../enums/depth-type';
 import { Period } from '../enums/period';
 import { Productionmonitoring } from '../models/productionmonitoring/productionmonitoring';
+import { UrlHelper } from '../shared/pipes/urlhelper';
 
 @Injectable({
   providedIn: 'root'
@@ -36,21 +37,21 @@ export class ProductionMonitoringService {
     params = params.append('ProjectId', projectId);
     params = params.append('WellId', wellId);
     params = params.append('DepthType', depthType);
-    return this.http.get(this.AppUrl + "wellboreProfile/zones", { params : params, headers: this.headers });
+    return this.http.get(this.AppUrl + UrlHelper.WellboreProfileZones, { params : params, headers: this.headers });
   }
 
   getPressureSensors(projectId: string, wellId: string): Observable<any> {
     let params = new HttpParams();
     params = params.append('ProjectId', projectId);
     params = params.append('WellId', wellId);
-    return this.http.get(this.AppUrl + "pressureFlowRate/pressure/sensors", {params : params, headers: this.headers });
+    return this.http.get(this.AppUrl + UrlHelper.PressureSensors, {params : params, headers: this.headers });
   }
 
   getFlowRateSensors(projectId: string, wellId: string): Observable<any> {
     let params = new HttpParams();
     params = params.append('ProjectId', projectId);
     params = params.append('WellId', wellId);
-    return this.http.get(this.AppUrl + "pressureFlowRate/flowRate/sensors", {params: params, headers: this.headers });
+    return this.http.get(this.AppUrl + UrlHelper.FlowRateSensors, {params: params, headers: this.headers });
   }
 
   getPressureHistoryData(
@@ -60,9 +61,7 @@ export class ProductionMonitoringService {
     periodicity : Period,
     SnapshotSize : number,
     fromDate: number | null,
-    toDate : number | null,
-    nativeFromDate : number | null,
-    nativeToDate : number | null
+    toDate : number | null
     ): Observable<any> {
     let params = new HttpParams();
     params = params.append('SensorId', sensorId);
@@ -72,9 +71,7 @@ export class ProductionMonitoringService {
     params = params.append('SnapshotSize', SnapshotSize);
     params = params.append('FromDate', fromDate);
     params = params.append('ToDate', toDate);
-    params = params.append('NativeFromDate', nativeFromDate);
-    params = params.append('NativeToDate', nativeToDate);
-    return this.http.get(this.AppUrl + "pressureFlowRate/pressure/historyData", {params: params, headers: this.headers });
+    return this.http.get(this.AppUrl + UrlHelper.PressureHistoryData, {params: params, headers: this.headers });
   }
 
   getFlowRateHistoryData(
@@ -84,9 +81,7 @@ export class ProductionMonitoringService {
     periodicity : Period,
     SnapshotSize : number,
     fromDate: number | null,
-    toDate : number | null,
-    nativeFromDate : number | null,
-    nativeToDate : number | null
+    toDate : number | null
     ): Observable<any> {
     let params = new HttpParams();
     params = params.append('SensorId', sensorId);
@@ -96,9 +91,7 @@ export class ProductionMonitoringService {
     params = params.append('SnapshotSize', SnapshotSize);
     params = params.append('FromDate', fromDate);
     params = params.append('ToDate', toDate);
-    params = params.append('NativeFromDate', nativeFromDate);
-    params = params.append('NativeToDate', nativeToDate);
-    return this.http.get(this.AppUrl + "pressureFlowRate/flowRate/historyData", {params: params, headers: this.headers });
+    return this.http.get(this.AppUrl + UrlHelper.FlowRateHistoryData, {params: params, headers: this.headers });
   }
 
   getZoneFlowProductionHistoryData(
@@ -110,9 +103,7 @@ export class ProductionMonitoringService {
     periodicity : Period,
     SnapshotSize : number,
     fromDate: number | null,
-    toDate : number | null,
-    nativeFromDate : number | null,
-    nativeToDate : number | null
+    toDate : number | null
     ): Observable<any> {
     let params = new HttpParams();
     params = params.append('SensorId', sensorId);
@@ -122,18 +113,16 @@ export class ProductionMonitoringService {
     params = params.append('SnapshotSize', SnapshotSize);
     params = params.append('FromDate', fromDate);
     params = params.append('ToDate', toDate);
-    params = params.append('NativeFromDate', nativeFromDate);
-    params = params.append('NativeToDate', nativeToDate);
     params = params.append('DepthType', depthType);
     params = params.append('ZoneNumber', zoneNumber);
-    return this.http.get(this.AppUrl + "zoneFlowProduction/historyData", { params: params, headers: this.headers });
+    return this.http.get(this.AppUrl + UrlHelper.ZoneFlowProductionHistoryData, { params: params, headers: this.headers });
   }
 
   GetZoneFlowProductionAcceptableLimits(projectId: string, wellId: string): Observable<any> {
     let params = new HttpParams();
     params = params.append('ProjectId', projectId);
     params = params.append('WellId', wellId);
-    return this.http.get(this.AppUrl + "zoneFlowProduction/criticalHighlights", { params: params, headers: this.headers });
+    return this.http.get(this.AppUrl + UrlHelper.ZoneFlowProductionAcceptableLimits, { params: params, headers: this.headers });
   }
 
 }
---
+++ src/app/shared/pipes/urlhelper.ts
@@ -0,0 +1,9 @@
+export  class UrlHelper {
+    public static readonly WellboreProfileZones : string  = "api/productionmonitoring/wellboreProfile/zones";
+    public static readonly PressureSensors : string  = "api/productionmonitoring/pressureFlowRate/pressure/sensors";
+    public static readonly FlowRateSensors : string  = "api/productionmonitoring/pressureFlowRate/flowRate/sensors";
+    public static readonly PressureHistoryData : string  = "api/productionmonitoring/pressureFlowRate/pressure/historyData";
+    public static readonly FlowRateHistoryData : string  = "api/productionmonitoring/pressureFlowRate/flowRate/historyData";
+    public static readonly ZoneFlowProductionHistoryData : string  = "api/productionmonitoring/zoneFlowProduction/historyData";
+    public static readonly ZoneFlowProductionAcceptableLimits : string  = "api/productionmonitoring/zoneFlowProduction/criticalHighlights";
+}
