8bd3fe7974fd8a7dec5c6fd84f3d0030de560a7f
Mykhailo Shuturma <Mykhailo.Shuturma2@halliburton.com>
2021-07-07 14:27:02 +0000

    Merged PR 14331: Hot fix: error on a production mode
    Hot fix: error on a production mode
    Minor chanes of the header

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  WSApiEndpoint: 'ws://localhost:5000//ws-api?SESSION-TOKEN=',
+  WSApiEndpoint: 'ws://localhost:5000/ws-api?SESSION-TOKEN=',
   API_URL: "http://localhost:5000/"
 };
