0f70b86e396da7fcbff3186c34ca4cf0d5756cee
Haykaz Sayadyan <54286866+hsayadyan@users.noreply.github.com>
2021-07-13 16:03:13 -0700

    added backend calls for table data

--- src/app/shared/components/well-sellection-with-buttons/dialog-table.component.ts
+++ src/app/shared/components/well-sellection-with-buttons/dialog-table.component.ts
@@ -1,52 +1,12 @@
 import { AfterViewInit, Component, ViewChild, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { WellboreSearchCommand } from '../../../models/Request/WellboreSearchCommand';
+import { WellboreSearchResponse } from '../../../models/Response/WellboreSearchResponse';
+import { ProductionMonitoringService } from '../../../services/productionMonitoring.service';
 import { PeriodicElement } from '../../../models/WellSelector'
-
-var CURRENT_PROJECT_ELEMENT_DATA: PeriodicElement[] = [
-  { Wellname: 'Well1', Padname: 'Hydrogen', Field: 'F1', Project: 'H' },
-  { Wellname: 'Well2', Padname: 'Helium', Field: 'F2', Project: 'He' },
-  { Wellname: 'Well3', Padname: 'Lithium', Field: 'F3', Project: 'Li' },
-  { Wellname: 'Well4', Padname: 'Beryllium', Field: 'F4', Project: 'Be' },
-  { Wellname: 'Well5', Padname: 'Boron', Field: 'F5', Project: 'B' },
-  { Wellname: 'Well6', Padname: 'Carbon', Field: 'F6', Project: 'C' },
-  { Wellname: 'Well7', Padname: 'Nitrogen', Field: 'F7', Project: 'N' },
-  { Wellname: 'Well8', Padname: 'Oxygen', Field: 'F8', Project: 'O' },
-  { Wellname: 'Well9', Padname: 'Fluorine', Field: 'F9', Project: 'F' },
-  { Wellname: 'Well10', Padname: 'Neon', Field: 'F10', Project: 'Ne' },
-];
-
-var RECENT_WELLS_ELEMENT_DATA: PeriodicElement[] = [
-  { Wellname: 'Well1', Padname: 'Hydrogen', Field: 'F1', Project: 'H' },
-  { Wellname: 'Well2', Padname: 'Helium', Field: 'F2', Project: 'He' },
-  { Wellname: 'Well3', Padname: 'Lithium', Field: 'F3', Project: 'Li' },
-  { Wellname: 'Well4', Padname: 'Beryllium', Field: 'F4', Project: 'Be' },
-  { Wellname: 'Well5', Padname: 'Boron', Field: 'F5', Project: 'B' },
- 
-];
-
-var ALL_WELLS_ELEMENT_DATA: PeriodicElement[] = [
-  { Wellname: 'Well1', Padname: 'Hydrogen', Field: 'F1', Project: 'H' },
-  { Wellname: 'Well2', Padname: 'Helium', Field: 'F2', Project: 'He' },
-  { Wellname: 'Well3', Padname: 'Lithium', Field: 'F3', Project: 'Li' },
-  { Wellname: 'Well4', Padname: 'Beryllium', Field: 'F4', Project: 'Be' },
-  { Wellname: 'Well5', Padname: 'Boron', Field: 'F5', Project: 'B' },
-  { Wellname: 'Well6', Padname: 'Carbon', Field: 'F6', Project: 'C' },
-  { Wellname: 'Well7', Padname: 'Nitrogen', Field: 'F7', Project: 'N' },
-  { Wellname: 'Well8', Padname: 'Oxygen', Field: 'F8', Project: 'O' },
-  { Wellname: 'Well9', Padname: 'Fluorine', Field: 'F9', Project: 'F' },
-  { Wellname: 'Well10', Padname: 'Neon', Field: 'F10', Project: 'Ne' },
-  { Wellname: 'Well11', Padname: 'Hydrogen', Field: 'F11', Project: 'H' },
-  { Wellname: 'Well12', Padname: 'Helium', Field: 'F12', Project: 'He' },
-  { Wellname: 'Well13', Padname: 'Lithium', Field: 'F13', Project: 'Li' },
-  { Wellname: 'Well14', Padname: 'Beryllium', Field: 'F14', Project: 'Be' },
-  { Wellname: 'Well15', Padname: 'Boron', Field: 'F15', Project: 'B' },
-  { Wellname: 'Well16', Padname: 'Carbon', Field: 'F16', Project: 'C' },
-  { Wellname: 'Well17', Padname: 'Nitrogen', Field: 'F17', Project: 'N' },
-  { Wellname: 'Well18', Padname: 'Oxygen', Field: 'F18', Project: 'O' },
-  { Wellname: 'Well19', Padname: 'Fluorine', Field: 'F19', Project: 'F' },
-  { Wellname: 'Well20', Padname: 'Neon', Field: 'F20', Project: 'Ne' },
-];
 
 @Component({
   selector: 'dialog-table',
@@ -54,34 +14,96 @@ var ALL_WELLS_ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: 'dialog-table.component.html',
   encapsulation: ViewEncapsulation.None
 })
-export class DialogTableComponent implements OnInit{
-  
-  
+export class DialogTableComponent implements OnInit {
+  public wellboreData: PeriodicElement[] = [];
   public selectedWell: string;
   public displayedColumns: string[] = ['Wellname', 'Padname', 'Field', 'Project'];
-  public currentProjectDataSource = new MatTableDataSource(CURRENT_PROJECT_ELEMENT_DATA);
-  public recentWellsDataSource = new MatTableDataSource(RECENT_WELLS_ELEMENT_DATA);
-  public allWellsDataSource = new MatTableDataSource(ALL_WELLS_ELEMENT_DATA);
+  public currentProjectDataSource = new MatTableDataSource(this.wellboreData);
+  public recentWellsDataSource = new MatTableDataSource(this.wellboreData);
+  public allWellsDataSource = new MatTableDataSource(this.wellboreData);
   public currentProject: string = "Current Project";
   public recentWells: string = "Recent Wells";
   public allWells: string = "All Wells";
+  private wellboreSearchCommand: WellboreSearchCommand;
+  private wellboreSearchReasponse: WellboreSearchResponse;
+  private isShowNearbyWellsOnly: boolean;
+  private isRecentWellsSelected: boolean;
+  private isCurrentProjectWellsSelected: boolean;
+  private isAllWellsSelected: boolean;
+  private searchtext: string;
+
+  constructor(public pmService: ProductionMonitoringService) { }
 
   ngOnInit(): void {
+    this.getWellData();
     this.currentProject = "Current Project " + "(" + this.currentProjectDataSource.data.length + ")";
     this.recentWells = "Recent Wells " + "(" + this.recentWellsDataSource.data.length + ")";
     this.allWells = "All Wells " + "(" + this.allWellsDataSource.data.length + ")";
   }
+
+  onShowNearbyWellsOnlyChange(ob: MatCheckboxChange) {
+    this.isShowNearbyWellsOnly = (this.isShowNearbyWellsOnly) ? false : true;
+    this.getWellData();
+  }
+
+  tabChanged(ob: MatTabChangeEvent) {
+    switch (ob.index) {
+      case 0:
+        this.isRecentWellsSelected = false;
+        this.isCurrentProjectWellsSelected = true;
+        this.isAllWellsSelected = false;
+        break;
+      case 1:
+        this.isRecentWellsSelected = true;
+        this.isCurrentProjectWellsSelected = false;
+        this.isAllWellsSelected = false;
+        break;
+      case 2:
+        this.isRecentWellsSelected = false;
+        this.isCurrentProjectWellsSelected = false;
+        this.isAllWellsSelected = true;
+        break;
+      default:
+        break;
+    }
+    this.getWellData();
+  }
+
+  public async getWellData() {
+    this.wellboreSearchCommand = {
+      CurrentProjectId: "projectid",
+      NearbyWellsOnly: this.isShowNearbyWellsOnly,
+      PageIndex: 1,
+      RecentWells: this.isRecentWellsSelected,
+      ResultsPerPage: 20,
+      SearchString: this.searchtext
+    };
+    await this.pmService.getWellboreSearch(this.wellboreSearchCommand).then(data => {
+      this.wellboreSearchReasponse = data;
+      this.wellboreSearchReasponse.result.forEach(element => {
+        this.wellboreData.forEach(wd => {
+          wd.Field = element.project.field,
+            wd.Padname = element.project.padName,
+            wd.Project = element.project.projectId,
+            wd.Wellname = element.wellbore.wellId
+        })
+      });
+
+    })
+  }
+
   public selected(row) {
     this.selectedWell = row.Wellname;
   }
 
   public applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
+    this.searchtext = filterValue.trim().toLowerCase();
     this.currentProjectDataSource.filter = filterValue.trim().toLowerCase();
     this.recentWellsDataSource.filter = filterValue.trim().toLowerCase();
     this.allWellsDataSource.filter = filterValue.trim().toLowerCase();
 
-    
+
     this.currentProject = "Current Project " + "(" + this.currentProjectDataSource.filteredData.length + ")";
     this.recentWells = "Recent Wells " + "(" + this.recentWellsDataSource.filteredData.length + ")";
     this.allWells = "All Wells " + "(" + this.allWellsDataSource.filteredData.length + ")";
