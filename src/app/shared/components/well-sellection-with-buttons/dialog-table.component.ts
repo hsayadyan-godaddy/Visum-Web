38503f39545026413676df16915849771bbc1989
Ravindra Deshmukh <Ravindra.Deshmukh@halliburton.com>
2021-07-22 12:27:17 +0530

    fixed PR comments

--- src/app/shared/components/well-sellection-with-buttons/dialog-table.component.ts
+++ src/app/shared/components/well-sellection-with-buttons/dialog-table.component.ts
@@ -5,7 +5,7 @@ import { PeriodicElement } from '../../../models/WellSelector';
 import { ProductionMonitoringService } from '../../../services/productionMonitoring.service';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { WellboreSearchResponse } from '../../../models/Response/WellboreSearchResponse';
-
+import {ProductionMonitoringConstants} from '../../../models/constants/production-monitoring'
 @Component({
   selector: 'dialog-table',
   styleUrls: ['dialog-table.component.scss'],
@@ -20,9 +20,9 @@ export class DialogTableComponent implements OnInit {
   pageSize: number = 15;
   pageIndex: number;
   private searchString: string;
-  public currentProject: string = "Current Project";
-  public recentWells: string = "Recent Wells";
-  public allWells: string = "All Wells";
+  public currentProject: string = ProductionMonitoringConstants.currentProject;
+  public recentWells: string = ProductionMonitoringConstants.recentWells;
+  public allWells: string = ProductionMonitoringConstants.allWells;
   constructor(public productionMonitoringService: ProductionMonitoringService) {
   }
 
@@ -49,9 +49,9 @@ export class DialogTableComponent implements OnInit {
   private initTable(command: WellboreSearchCommand) {
     (async (context) => {
       const response: WellboreSearchResponse = await context.productionMonitoringService.getWellboreSearch(command);
-      this.currentProject = "Current Project " + "(" + response.result.length + ")";
-      this.recentWells = "Recent Wells " + "(" + response.result.length + ")";
-      this.allWells = "All Wells " + "(" + response.result.length + ")";
+      this.currentProject = ProductionMonitoringConstants.currentProject + "(" + response.result.length + ")";
+      this.recentWells = ProductionMonitoringConstants.recentWells + "(" + response.result.length + ")";
+      this.allWells = ProductionMonitoringConstants.allWells + "(" + response.result.length + ")";
       context.dataSource = new MatTableDataSource<PeriodicElement>(response
         .result.map(x => {
           const item: PeriodicElement = {
